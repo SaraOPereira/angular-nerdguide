817cc912ebc38921125bf8021be391316d2d31d6
Sara Pereira <sara.novaalianca@hotmail.com>
2022-10-17 20:21:22 -0300

    home, navbar and footer

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  private token = "5456885634395471";
+
+  charOne: any; charTwo: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://www.superheroapi.com/api.php/' + this.token + '/70').subscribe(
+      charOne => this.charOne = charOne
+    );
+    this.http.get('https://www.superheroapi.com/api.php/' + this.token + '/346').subscribe(
+      charTwo => this.charTwo = charTwo
+    );
+  }
+
+}
